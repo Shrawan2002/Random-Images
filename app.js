6879dd647c28d8df49d2794d2bed9c1cc7c59d24
Shrawan2002 <shrawankumar1754@gmail.com>
2024-05-06 11:42:52 +0530

    initial files

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 let url ="https://api.thecatapi.com/v1/images/search";
 let btn= document.querySelector("button");
+btn.addEventListener("click", async ()=>{
+    let link= await getImg();
+    let img=document.querySelector("#result");
+    img.setAttribute("src",link);
+    console.log(link);
+})
+
+
+async function getImg() {
+    let res= await axios.get(url);
+    console.log(res.data[0].url);
+    return res.data[0].url;
+    
+}
