ad0932cc93dc118e2a94f19a66d629de5547fa30
Shrawan2002 <shrawankumar1754@gmail.com>
2024-05-06 11:29:08 +0530

    update files

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
-let url="https://api.thecatapi.com/v1/images/search?limit=10";
-
-
+let url ="https://api.thecatapi.com/v1/images/search";
 let btn= document.querySelector("button");
-
-btn.addEventListener("click", async function(){
-    let pra = document.querySelector("#fact");
-    let fact= await getFact();
-    console.log(fact);
-    pra.innerText=fact;
-
-})
-
-async function getFact(){
-    let response=await axios.get(url);
-    return response;
-}
